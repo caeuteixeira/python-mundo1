768bd75165c8a9f8872ba9b255b3841b94837c92
Caeu <caeufertte@gmail.com>
2023-09-05 22:33:43 -0300

    first commit

---
+++ aula-4.py
@@ -0,0 +1,15 @@
+#desafio 1
+nome = input ('Qual seu nome?')
+print('Olá', nome, 'prazer em te conhecer!')
+
+#desafio 2
+dia = input ('Em que dia você nasceu?')
+mes = input ('Em que mês você nasceu?')
+ano = input ('Em que ano você nasceu?')
+print('Você nasceu em', dia, '/', mes, '/', ano,'correto?')
+
+#desafio 3
+numero1 = input ('Primeiro número')
+numero2 = input ('Segundo número')
+soma = int(numero1) + int(numero2)
+print('A soma desses números é', soma)
